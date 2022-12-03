123555fa59cff83a4e9aef3a508dac3f7793e35d
4everVortex <119734721+4everVortex@users.noreply.github.com>
2022-12-03 19:35:00 +0700

    Update LibDrip.lua

--- LibDrip.lua
+++ LibDrip.lua
@@ -26,18 +26,14 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(29, 29, 36);
-    BackgroundColor = Color3.fromRGB(26, 29, 34);
+    MainColor = Color3.fromRGB(28, 28, 28);
+    BackgroundColor = Color3.fromRGB(20, 20, 20);
     AccentColor = Color3.fromRGB(119, 0, 255);
-    OutlineColor = Color3.fromRGB(79, 99, 99);
+    OutlineColor = Color3.fromRGB(50, 50, 50);
 
     Black = Color3.new(0, 0, 0);
 
     OpenedFrames = {};
-
-    Signals = {};
-    ScreenGui = ScreenGui;
-
 };
 
 task.spawn(function()
